a411a2f1910d0b243128113cf38d89616ac1fe8d
rheber <rheber@rediffmail.com>
2017-09-25 12:46:44 +1000

    Read keys file

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use std::env;
+use std::fs::File;
+use std::io;
+use std::io::Read;
 
 #[cfg(test)]
 mod tests {
@@ -7,12 +10,24 @@ mod tests {
     }
 }
 
-pub fn getWord(mut args: env::Args) -> Result<String, &'static str> {
+pub fn get_word(mut args: env::Args) -> Result<String, &'static str> {
   args.next(); // Discard program name.
 
   args.next().ok_or("Missing headword argument")
 }
 
-pub fn run(word: String) {
+fn read_file() -> Result<String, io::Error> {
+  let mut file = File::open("keys.txt")?;
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)?;
+
+  Ok(contents)
+}
+
+pub fn run(word: String) -> Result<(), io::Error> {
   println!("{}", word);
+  let key_string = read_file()?;
+  println!("{}", key_string);
+
+  Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use std::env;
 use std::process;
 
 fn main() {
-  let word = gloss::getWord(env::args()).unwrap_or_else(|err| {
+  let word = gloss::get_word(env::args()).unwrap_or_else(|err| {
     eprintln!("Error: {}", err);
     process::exit(1);
   });
-  gloss::run(word);
+
+  gloss::run(word).unwrap_or_else(|err| {
+    eprintln!("Error: {}", err);
+    process::exit(1);
+  });
 }
