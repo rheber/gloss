3290f472375cc8ec154f1f53a216eca036d207ac
rheber <rheber@rediffmail.com>
2017-09-25 11:35:57 +1000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
+
+pub fn run() {
+  println!("Hello world!");
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+extern crate gloss;
+
+fn main() {
+  gloss::run();
+}
