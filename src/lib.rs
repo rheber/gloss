31ba8ec2d76bbc3f33a268f82bea3f4f58137581
rheber <rheber@rediffmail.com>
2017-09-28 23:33:59 +1000

    get_response

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 extern crate reqwest;
-use reqwest::{Client, Request, Url};
+use reqwest::{Client, Response, Url};
+
+#[macro_use] extern crate hyper;
+use hyper::header::Headers;
 
 use std::collections::HashMap;
 use std::env;
@@ -50,26 +53,38 @@ fn parse_keys(key_string: &str) -> Result<HashMap<&str, &str>, &'static str> {
   Ok(keys)
 }
 
-fn construct_request(word: &str, keys: HashMap<&str, &str>) ->
-  Result<Request, Box<Error>> {
+fn get_response(word: &str, keys: HashMap<&str, &str>) ->
+  Result<Response, Box<Error>> {
   let base_url = keys.get("base_url=").ok_or("Missing base_url key")?;
   let app_key = keys.get("app_key=").ok_or("Missing app_key key")?;
   let app_id = keys.get("app_id=").ok_or("Missing app_id key")?;
   let url = (String::from(*base_url) + word).parse::<Url>().unwrap();
 
-  let req = Client::new()?.
-    post(url)?.
-    form(&[("app_key", *app_key), ("app_id", *app_id)])?.
-    build();
-  Ok(req)
+  header! { (Appkey, "app_key") => [String] }
+  header! { (Appid, "app_id") => [String] }
+  let mut heads = Headers::new();
+  heads.set(Appkey(String::from(*app_key)));
+  heads.set(Appid(String::from(*app_id)));
+
+  let resp = Client::new()?.
+    get(url)?.
+    headers(heads).
+    send();
+
+  match resp {
+    Err(e) => Err(Box::new(e)),
+    Ok(a) => Ok(a),
+  }
 }
 
 pub fn run(word: &str) -> Result<(), Box<Error>> {
-  println!("{}", word);
   let key_string = read_file()?;
   let key_pairs = parse_keys(&key_string[..])?;
-  let req = construct_request(word, key_pairs);
-  println!("{:?}", req);
+  let mut resp = get_response(word, key_pairs)?;
+  println!("{:?}", resp);
+  let mut content = String::new();
+  resp.read_to_string(&mut content)?;
+  println!("{}", content);
 
   Ok(())
 }
