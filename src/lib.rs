31caa03303698c637ff83c7b3d72d30db4f08f95
rheber <rheber@rediffmail.com>
2017-10-01 17:10:09 +1100

    serde

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,14 @@ use reqwest::{Client, Response, Url};
 #[macro_use] extern crate hyper;
 use hyper::header::Headers;
 
+extern crate serde_json;
+
 use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io;
-use std::io::Read;
+use std::io::prelude::*;
 
 #[cfg(test)]
 mod tests {
@@ -24,8 +26,8 @@ pub fn get_word(mut args: env::Args) -> Result<String, &'static str> {
   args.next().ok_or("Missing headword argument")
 }
 
-fn read_file() -> Result<String, io::Error> {
-  let mut file = File::open("keys.txt")?;
+fn read_file(filename: &str) -> Result<String, io::Error> {
+  let mut file = File::open(filename)?;
   let mut contents = String::new();
   file.read_to_string(&mut contents)?;
 
@@ -77,14 +79,36 @@ fn get_response(word: &str, keys: HashMap<&str, &str>) ->
   }
 }
 
+fn read_glosses(text: &str) -> Result<HashMap<&str, &str>, Box<Error>> {
+  serde_json::from_str(text).or({
+    Ok(HashMap::new())
+  })
+}
+
+fn save_glosses(glosses: HashMap<&str, &str>) -> Result<(), Box<Error>> {
+  let serial = serde_json::to_string(&glosses)?;
+  let mut gloss_file = File::create("glosses")?;
+  gloss_file.write_all(serial.as_bytes())?;
+
+  Ok(())
+}
+
 pub fn run(word: &str) -> Result<(), Box<Error>> {
-  let key_string = read_file()?;
+  let key_string = read_file("keys.txt")?;
   let key_pairs = parse_keys(&key_string[..])?;
-  let mut resp = get_response(word, key_pairs)?;
-  println!("{:?}", resp);
-  let mut content = String::new();
-  resp.read_to_string(&mut content)?;
-  println!("{}", content);
+
+  let glosses_result: Result<String, &'static str> = read_file("glosses").or({
+    File::create("glosses")?;
+    Ok(String::new())
+  });
+  let glosses_unwrapped = glosses_result.unwrap();
+  let glossmap = read_glosses(&glosses_unwrapped[..])?;
+//  let mut resp = get_response(word, key_pairs)?;
+//  println!("{:?}", resp);
+//  let mut content = String::new();
+//  resp.read_to_string(&mut content)?;
+//  println!("{}", content);
+  save_glosses(glossmap)?;
 
   Ok(())
 }
