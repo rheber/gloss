8e12b4a1aba5edd0d87937d1142452ed16df44d5
rheber <rheber@rediffmail.com>
2017-10-22 11:27:34 +1100

    Read and store definitions
    Commented some functions.
    Tweaked hashmap type and get_response signature.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ extern crate serde_json;
 use std::collections::HashMap;
 use std::env;
 use std::error::Error;
+use std::fmt;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
@@ -20,6 +21,24 @@ mod tests {
     }
 }
 
+#[derive(Debug)]
+struct GlossError {
+  err_string: String
+}
+
+impl fmt::Display for GlossError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "GlossError")
+  }
+}
+
+impl Error for GlossError {
+  fn description(&self) -> &str {
+    &self.err_string[..]
+  }
+}
+
+// Get command line argument.
 pub fn get_word(mut args: env::Args) -> Result<String, &'static str> {
   args.next(); // Discard program name.
 
@@ -55,8 +74,11 @@ fn parse_keys(key_string: &str) -> Result<HashMap<&str, &str>, &'static str> {
   Ok(keys)
 }
 
-fn get_response(word: &str, keys: HashMap<&str, &str>) ->
-  Result<Response, Box<Error>> {
+// Submit API request and return response.
+fn get_response(word: &str) -> Result<Response, Box<Error>> {
+  let key_string = read_file("keys.txt")?;
+  let keys = parse_keys(&key_string[..])?;
+
   let base_url = keys.get("base_url=").ok_or("Missing base_url key")?;
   let app_key = keys.get("app_key=").ok_or("Missing app_key key")?;
   let app_id = keys.get("app_id=").ok_or("Missing app_id key")?;
@@ -79,13 +101,15 @@ fn get_response(word: &str, keys: HashMap<&str, &str>) ->
   }
 }
 
-fn read_glosses(text: &str) -> Result<HashMap<&str, &str>, Box<Error>> {
+// Read map of glosses from serialised string.
+fn read_glosses(text: &str) -> Result<HashMap<&str, String>, Box<Error>> {
   serde_json::from_str(text).or({
     Ok(HashMap::new())
   })
 }
 
-fn save_glosses(glosses: HashMap<&str, &str>) -> Result<(), Box<Error>> {
+// Write map of glosses to file.
+fn save_glosses(glosses: HashMap<&str, String>) -> Result<(), Box<Error>> {
   let serial = serde_json::to_string(&glosses)?;
   let mut gloss_file = File::create("glosses")?;
   gloss_file.write_all(serial.as_bytes())?;
@@ -93,21 +117,35 @@ fn save_glosses(glosses: HashMap<&str, &str>) -> Result<(), Box<Error>> {
   Ok(())
 }
 
-pub fn run(word: &str) -> Result<(), Box<Error>> {
-  let key_string = read_file("keys.txt")?;
-  let key_pairs = parse_keys(&key_string[..])?;
+// Retrieve gloss from map, requesting and inserting it if absent.
+fn get_gloss<'a, 'b>(word: &'b str, glosses: &'a mut HashMap<&'b str, String>) ->
+  Result<&'a String, Box<Error>> {
+  // Used even though we know a gloss exists to satisfy types.
+  let impossible_error =
+    Box::new(GlossError {err_string: String::from("Expected gloss in map.")});
 
+  if glosses.contains_key(word) {
+    return glosses.get(word).ok_or(impossible_error);
+  }
+  let mut resp = get_response(word)?;
+  let mut content = String::new();
+  resp.read_to_string(&mut content)?;
+  glosses.insert(word, content);
+
+  glosses.get(word).ok_or(impossible_error)
+}
+
+pub fn run(word: &str) -> Result<(), Box<Error>> {
   let glosses_result: Result<String, &'static str> = read_file("glosses").or({
     File::create("glosses")?;
     Ok(String::new())
   });
   let glosses_unwrapped = glosses_result.unwrap();
-  let glossmap = read_glosses(&glosses_unwrapped[..])?;
-//  let mut resp = get_response(word, key_pairs)?;
-//  println!("{:?}", resp);
-//  let mut content = String::new();
-//  resp.read_to_string(&mut content)?;
-//  println!("{}", content);
+  let mut glossmap = read_glosses(&glosses_unwrapped[..])?;
+{
+  let resp = get_gloss(word, &mut glossmap)?;
+  println!("{}", resp);
+}
   save_glosses(glossmap)?;
 
   Ok(())
